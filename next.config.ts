0a7d59a9ef95f81df0b7ad2b808723001dcbb374
Jose Chaves <jchaves10@gmail.com>
2025-08-26 16:33:14 +0000

    primero que nada me lanza un mensaje que el website no es seguro y luego

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  // `output: 'export'` is auto-configured by App Hosting.
+  // The `trailingSlash` option is needed for it to be compatible with
+  // Netlify and other static hosts.
+  trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true,
