6b0fb9f56dc0b6278f97e28258fb10644145d00b
Jose Chaves <jchaves10@gmail.com>
2025-08-26 17:50:34 +0000

    ok con esta configuracion al darle enviar postulacion, los datos se envi

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    // This allows requests from the preview deployment to be authorized.
+    // The previous value was:
+    // allowedDevOrigins: ['*'],
+    // This is not a security risk because it only applies to development.
+    allowedDevOrigins: [
+      'https://*.google.com',
+      'https://*.cloud.google.com',
+      'https://*.cloud.run',
+    ],
+  },
 };
 
 export default nextConfig;
