58533c2cc8163b838e039c1a4eaaddebc6e0c6db
Jose Chaves <jchaves10@gmail.com>
2025-10-03 22:03:05 +0000

    aun sigue saliendo mal el logo, podemos usar el favicon?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { LanguageProvider } from '@/context/language-context';
 
-const logoUrl = '/images/preview.png';
+const logoUrl = '/images/Favicon.png';
 
 export const metadata: Metadata = {
   title: 'Producción Audiovisual y Videos Profesionales en Costa Rica | Videame',
